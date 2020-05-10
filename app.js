b227331495bb4d38258a406860357324e65c9072
Hadi Arbabi <h.arbabi@banicm.com>
2020-05-10 14:42:24 +0430

    prophet

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-class Person {
+class AgedPerson {
+  printAge() {
+    console.log(this.age);
+  }
+}
+class Person extends AgedPerson {
   name = "max";
+
   constructor() {
+    super();
     this.age = 12;
   }
 
@@ -9,6 +16,30 @@ class Person {
   }
 }
 
-const person = new Person();
+// function Person() {
+//   this.age = 12;
+//   this.name = "max";
+//   this.greet = function () {
+//     console.log(
+//       "hi i am " + this.name + " and i am " + this.age + " years old"
+//     );
+//   };
+// }
 
-person.greet();
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   },
+// };
+Person.prototype.printAge = function () {
+  console.log(this.age);
+};
+
+const p = new Person();
+
+p.greet();
+p.printAge();
+console.log(p.__proto__);
+console.log(p.toString());
+
+console.dir(Object);
